c9d8743292ce5f2e525b903d323f4cc02f1b8d42
Aziz <azizanarov1908@gmail.com>
2024-08-22 23:08:45 +0500

    js-3

---
+++ main.js
@@ -0,0 +1,34 @@
+// Задание 1
+
+// let num =+prompt('Укажите кол-во бананов') 
+// for(let i = 1; i <= num; i++) {
+//     if(i==1) {
+//         console.log(i + ' banana');
+//     }else {
+//         console.log(i + ' bananas')
+//     }
+// }
+
+// Задание 2
+
+//  let num =+prompt('Введите любое число')
+// let sum = 0
+// for(let i=0; i<=num; i++) {
+//     if(i % 2 === 0) {
+//     sum = sum + i;
+//     }
+// }
+// console.log(sum); 
+
+
+// Задание 3
+
+// let num =+prompt('Введите число')
+// let degree =+prompt('Введите степень')
+// let answer = 1
+
+
+// for(let i = 0; i < degree; i++) {
+//     answer = answer * num
+// }
+// alert(answer);
